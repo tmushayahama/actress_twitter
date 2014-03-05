a7adafffa80eca52f429c8a730b4cc8adbf5aa05
tmushayahama <tmushayahama@gmail.com>
2014-03-04 22:27:37 -0500

    Created a rails app. I am using aptana studio for the work.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ActressTwitter::Application.config.secret_key_base = 'b68e6e439a185b3e4e637ee5dcc6bd9395b8cb98054e1eb7dda429d83d7a6e5fd66035271fee53536f47bb070c715547a764b4ac194d757c02d657be9f4be42e'
