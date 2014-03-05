e8b912af7444c909de47d99a84be96906b95d34c
tmushayahama <tmushayahama@gmail.com>
2014-03-05 04:06:22 -0500

    Added a twitter search for Selena Gomez. It scrap her twitter account. I searched on twitter to see a verified account. This is done by looking for icon and verified class. Normally it will be the first one retrieved by the nokogiri, then simply get the next element which contains the twitter name. The file ruby file is under scrapper/scrap_selena_gomez_twitter

--- scrapper/scrap_actresses.rb
+++ scrapper/scrap_actresses.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'openssl'
 
 url = "http://en.wikipedia.org/wiki/List_of_American_film_actresses"
 data = Nokogiri::HTML(open(url))
@@ -7,3 +8,7 @@ actress_list =  data.css(".div-col li")
 actress_list.each do |actress|
 	puts actress.at_css('a').text.strip
 end
+
+url2 = "https://twitter.com/search?q=Selena%20Gomez&mode=users"
+data2 = Nokogiri::HTML(open(url2, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+puts data2.css(".verified")[1].next_element.text
